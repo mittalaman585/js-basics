8d42bc99202a9c13992a2934e1a05d66a68e32c1
Aman Mittal <51812406+mittalaman585@users.noreply.github.com>
2024-02-24 07:20:15 +0000

    variables

---
+++ basicsjs/variables.js
@@ -0,0 +1,6 @@
+const account = 2245001
+let accountEmail = "mittalaman585"
+var accountPassword = "124567"
+accountCity = "ludhiana"
+
+console.table([account,accountEmail,accountPassword,accountCity])
