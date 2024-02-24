8ee764db1717b34f859b906b432eeb41ed4cecea
Aman Mittal <51812406+mittalaman585@users.noreply.github.com>
2024-02-24 07:28:53 +0000

    varaible notes

--- basicsjs/variables.js
+++ basicsjs/variables.js
@@ -3,4 +3,7 @@ let accountEmail = "mittalaman585"
 var accountPassword = "124567"
 accountCity = "ludhiana"
 
+// prefer not to use var because of block scope and function scope 
+//we can use console.log() for inle statemnet to run it on multiple statement one need to use consol.table for table representation
 console.table([account,accountEmail,accountPassword,accountCity])
+console.log(accountEmail)
